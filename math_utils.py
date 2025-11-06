202925abbee574c3385a755724e1a1b38809b01b
noammelusyan-del <noammelusyan@gmail.com>
2025-11-06 08:29:18 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -1,14 +1,21 @@
-def find_max_number (num1, num2, num3):
-    if num1 >= num2 and num1 >= num3:
-      return num1
-    elif num2 >= num1 and num2 >= num3:
-      return num2
-    else:
-     return num3 
-def find_mean (num1, num2 , num3):
-    return (num1 + num2 + num3) / 3
-def find_mean_std (num1, num2, num3):
-    mean = find_mean (num1, num2 ,num3)
-    variance = ((num1 - mean) ** 2 +(num2 - mean) ** 2 (num3 - mean) ** 2 ) / 3
-    std = (variance) ** 0.5
+
+def find_max_number(n1, n2, n3):
+    if n1 > n2 :
+      if n1 > n3 :
+        return n1
+      else :
+        return n3
+    else :
+      if n2 > n3 :
+        return n2
+      else :
+        return n3
+
+def find_mean(num1, num2, num3):
+    mean = (num1 + num2 + num3)/3
+    return mean
+
+def find_mean_std(num1, num2, num3):
+    mean = find_mean(num1, num2, num3)
+    std = (((num1 - mean)**2 + (num2 -mean)**2 +(num3 - mean)**2)/3)**0.5
     return mean, std
